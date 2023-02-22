0ccc65a5fb112faa7ea50d39cb640ab122e10889
Oleksandr Kivalo <113942764+Sanyakila@users.noreply.github.com>
2023-02-22 23:12:40 +0200

    4

--- js/task-08.js
+++ js/task-08.js
@@ -2,12 +2,18 @@ const formRef = document.querySelector('.login-form');
 
 formRef.addEventListener('submit', (event) => {
     event.preventDefault();
+    
     const { email, password } = event.currentTarget.elements;
 
     if (email.value === '' || password.value === '') {
         alert("Всі поля мають бути заповнені!");
     } else {
-    console.log(`Email: ${email.value}, Password: ${password.value}`);
+        const formData = {
+            email: email.value,
+            password: password.value
+        }
+    console.log(formData);
     event.currentTarget.reset();
     }
 });
+
