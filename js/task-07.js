56e3d4f5db5a7c6215af505e7f61204e2c090357
Oleksandr Kivalo <113942764+Sanyakila@users.noreply.github.com>
2024-02-01 11:34:47 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,13 @@
 const input = document.querySelector('#font-size-control');
 const output = document.querySelector('#text');
 
-output.style.fontSize = input.value + 'px';
+// output.style.fontSize = 3*input.value + 'px'; // 213px
+// console.log("OUTPUT:", output.style);
 
 const changeSizingOfText = () => {
     output.style.fontSize = `${input.value}px`;
 };
+
+changeSizingOfText();
 
 input.addEventListener('input', changeSizingOfText);
